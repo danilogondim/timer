01a71bb7243c1764f971d0d2ff949fb57c851fe4
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-01 12:27:43 +0000

    Add an interactive version of timer.js

--- timer2.js
+++ timer2.js
@@ -1,26 +1,20 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const stdin = process.stdin;
 
+console.log('Please use one of the following commands:');
+console.log('   -Press "b" for a immediate beep.');
+console.log('   -Press a number between 1 and 9 to set a timer for the specific period, in seconds.');
+console.log('   -Use Ctrl+c to exit the program.');
 
-rl.question('Please enter a command (b => beep right away; 0-9 => sets timer for x seconds): ', (answer) => {
-  if (answer.toLowerCase() === "b") {
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+stdin.resume();
+stdin.on('data', data => {
+  if (data === '\u0003') {
+    process.exit();
+  } else if (data.toLowerCase() === "b") {
     process.stdout.write('\x07');
-  } else if (answer === '\u0003') {
-    rl.close();
-  } else if (Number(answer) && Number(answer) >= 0) {
-
-    process.stdout.write(`setting timer for ${answer} seconds...`);
-    setTimeout(() => process.stdout.write('\x07'), Number(answer) * 1000);
+  } else if (Number(data) && Number(data) > 0) {
+    process.stdout.write(`setting timer for ${data} seconds...\n`);
+    setTimeout(() => process.stdout.write('\x07'), Number(data) * 1000);
   }
-
 });
-
-
-// for (const time of input) {
-//   if (Number(time) === 0 || (Number(time) && Number(time) >= 0)) {
-//     setTimeout(() => process.stdout.write('\x07'), Number(time) * 1000);
-//   }
-// }
